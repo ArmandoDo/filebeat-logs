f083c1175f516588c2de4d86029d373d8f309abe
Armando Dominguez <jorgearmds@gmail.com>
2025-04-08 09:53:12 -0600

    Support for Darwin (MacOS) added in repo

--- install-filebeat.sh
+++ install-filebeat.sh
@@ -5,38 +5,70 @@
 ##
 ## Usage:
 ## ./install-filebeat.sh
-
-# Load env variables
-source .env
-
-### Delete the character "/" in the end of the string
-export LOGS_FOLDER="${LOGS_FOLDER%/}"
+# set -ex
 
 ## Container registry
 export REGISTRY_NAME="development"
 # Filebeat
-export APP_NAME="filebeat-${ELASTIC_INDEX}"
 export APP_TAG="8.6.2"
 
 ## Get the OS
 OS_TYPE=$(uname)
 
-generate_docker_compose_file() {
-    envsubst < docker-compose.template.yml > docker-compose-${APP_NAME}.yml
-}
-
 ## Install docker image on linux
-install_on_linux() {
-    generate_docker_compose_file
+install_filebeat() {
     stop_container
-    docker compose --file docker-compose-${APP_NAME}.yml up --detach ${APP_NAME}
+    ${DOCKER_COMPOSE_COMMAND} --file ${DOCKER_COMPOSE_FILE} up --detach ${APP_NAME}
 
 }
 
-### Stop docker container
+# Set up the docker compose command available on the server
+set_up_docker_compose_command() {
+    if command -v docker-compose &> /dev/null; then
+        export DOCKER_COMPOSE_COMMAND="docker-compose"
+    elif command -v docker compose &> /dev/null; then
+        export DOCKER_COMPOSE_COMMAND="docker compose"
+    else
+        echo "Docker Compose is not installed or started in your system.
+        Please install the service."
+        echo "Exiting..."
+
+        exit 1
+    fi
+}
+
+# Set up the environment variable file .env
+set_up_env_variable_file() {
+    if ! source .env; then
+        echo "Missing .env file with the environment variables. Please create the .env file"
+        exit 1
+    fi
+    
+    ### Delete the character "/" in the end of the string
+    export LOGS_FOLDER="${LOGS_FOLDER%/}"
+}
+
+## Stop docker container
 stop_container() {
-    docker compose --file docker-compose-${APP_NAME}.yml stop ${APP_NAME}
-    docker compose --file docker-compose-${APP_NAME}.yml rm --force ${APP_NAME}
+    ${DOCKER_COMPOSE_COMMAND} --file ${DOCKER_COMPOSE_FILE} stop ${ELASTICSEARCH_APP_NAME}
+    ${DOCKER_COMPOSE_COMMAND} --file ${DOCKER_COMPOSE_FILE} rm --force ${ELASTICSEARCH_APP_NAME}
+}
+
+# Verify if the Docker engine is installed on the system
+verify_docker_engine() {
+    # Verify if service is installed
+    if ! command -v docker &> /dev/null; then
+        echo "Docker Engine is not installed in your system. Please install the service..."
+        echo "Exiting..."
+        exit 1
+    fi
+
+    # Verify if service is running
+    if ! docker info &> /dev/null; then
+        echo "Docker engine is installed, but not started. Please launch the service..."
+        echo "Exiting..."
+        exit 1
+    fi
 }
 
 ## Main function
@@ -45,10 +77,26 @@ main() {
     # Verify the OS
     case "${OS_TYPE}" in
         "Darwin")
-            echo "install_darwin"
+            verify_docker_engine
+            set_up_docker_compose_command
+            set_up_env_variable_file
+            # Docker compose filename
+            export APP_NAME="filebeat-${ELASTIC_INDEX}"
+            export DOCKER_COMPOSE_FILE="docker-compose-${ELASTIC_INDEX}.darwin.yml"
+            # Replace env variable
+            sed -e "s|\${ELASTIC_INDEX}|$ELASTIC_INDEX|g" docker-compose.darwin.template.yml > ${DOCKER_COMPOSE_FILE}
+            install_filebeat
             ;;
         "Linux")
-            install_on_linux
+            verify_docker_engine
+            set_up_docker_compose_command
+            set_up_env_variable_file
+            # Docker compose filename
+            export APP_NAME="filebeat-${ELASTIC_INDEX}"
+            export DOCKER_COMPOSE_FILE="docker-compose-${ELASTIC_INDEX}.ubuntu.yml"
+            # Replace env variables
+            envsubst < docker-compose.ubuntu.template.yml > ${DOCKER_COMPOSE_FILE}
+            install_filebeat
             ;;
         *)
             echo "System isn't supported by this script: ${OS_TYPE}"
